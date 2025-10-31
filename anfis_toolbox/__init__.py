ba0604336d1579ae5833dd5a8f1f081b402c4429
dcruzf <36683332+dcruzf@users.noreply.github.com>
2025-10-31 18:46:24 +0000

    chore: bump version to 0.1.2
    [publish]

--- anfis_toolbox/__init__.py
+++ anfis_toolbox/__init__.py
@@ -1,6 +1,6 @@
 """ANFIS Toolbox - A Python toolbox for Adaptive Neuro-Fuzzy Inference Systems."""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 # Expose high-level estimators
 from .classifier import ANFISClassifier
